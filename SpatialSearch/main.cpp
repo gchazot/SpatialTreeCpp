2a955af2c4440ebc56d29947617ce5507957fa99
Germain CHAZOT <g.chazot@gmail.com>
2017-12-03 10:10:13 -0500

    Some cleanup in main

--- SpatialSearch/main.cpp
+++ SpatialSearch/main.cpp
@@ -8,6 +8,8 @@
 #include <math.h>
 #include <limits>
 
+#include "SpatialTree.h"
+
 using namespace std;
 
 #define debug(message) cerr << message << endl
@@ -15,6 +17,7 @@ using namespace std;
 
 
 static const double PI = 3.14159265358979323846;
+static const double earthRadiusKm = 6371.0;
 
 double deg2rad(double deg) {
 	return (deg * PI / 180);
@@ -23,7 +26,6 @@ double deg2rad(double deg) {
 double rad2deg(double rad) {
 	return (rad * 180 / PI);
 }
-
 
 struct Flight {
 	Flight(size_t index, string callSign, double latitude, double longitude) :
@@ -34,9 +36,7 @@ struct Flight {
 	double latitude;
 	double longitude;
 
-	double distance(const Flight & other) {
-		static const double earthRadiusKm = 6371.0;
-
+	double distance(const Flight & other) const {
 		// Credits to https://stackoverflow.com/questions/10198985/calculating-the-distance-between-2-latitudes-and-longitudes-that-are-saved-in-a
 		double lat1r, lon1r, lat2r, lon2r, u, v;
 		lat1r = deg2rad(latitude);
@@ -63,27 +63,26 @@ Flight parse_line(const string line) {
 	const auto callSign = line.substr(0, comma1 - line.begin());
 	const auto latitude = line.substr(comma1 - line.begin() + 1, comma2 - comma1 - 1);
 	const auto longitude = line.substr(comma2 - line.begin() + 1, line.end() - comma2);
-
-	return Flight(0, move(callSign), stod(latitude), stod(longitude));
+	
+	try {
+		double lat = stod(latitude);
+		double lon = stod(longitude);
+		return Flight(0, move(callSign), lat, lon);
+	}
+	catch (invalid_argument e) {
+		throw runtime_error("Unable to parse " + line);
+	}
 }
 
+vector<Flight> parseFile(string fileName) {
+	vector<Flight> flights;
 
-
-
-int main()
-{
-	ifstream input_file("code_challenge/20170901_080005.csv");
-
+	ifstream input_file(fileName);
 	if (!input_file.is_open()) {
-		return 1;
+		return flights;
 	}
 
-	map< string, size_t > indices;
-	vector< Flight > flights;
-
-	// parse the file
 	string line;
-	getline(input_file, line); // ignore first line;
 	for (size_t n = 0; getline(input_file, line);) {
 		try {
 			Flight newFlight = parse_line(line);
@@ -91,12 +90,6 @@ int main()
 			++n;
 
 			flights.push_back(move(newFlight));
-			const auto & flight = flights.back();
-
-			const auto indexResult = indices.insert(make_pair(flight.callSign, n));
-			// if (!indexResult.second) {
-			// 	throw runtime_error("Duplicate entry " + flight.callSign);
-			// }
 		}
 		catch (runtime_error e) {
 			debug(e.what());
@@ -104,13 +97,18 @@ int main()
 		}
 	}
 
-	if (flights.size() < 2) {
-		// No reason to calculate distances
-		return 0;
-	}
+	return flights;
+}
 
-	cout << "Blabla" << endl;
+void print_result(const Flight & flight1, const Flight & flight2) {
+	cout << setfill(' ');
+	cout << setw(8) << left << flight1.callSign << " ";
+	cout << setw(8) << left << flight2.callSign << " ";
+	cout << setw(8) << right << fixed << setprecision(2) << flight1.distance(flight2);
+	cout << endl;
+}
 
+void SolveWithForForLoop(vector<Flight> flights) {
 	for (auto flight1 = flights.begin(); flight1 != flights.end(); ++flight1) {
 		Flight * closest = nullptr;
 		double min_distance = numeric_limits<double>::max();
@@ -127,14 +125,21 @@ int main()
 			}
 		}
 
-		cout << setfill(' ');
-		cout << setw(8) << left << flight1->callSign << " ";
-		cout << setw(8) << left << closest->callSign << " ";
-		cout << setw(8) << right << fixed << setprecision(2) << min_distance;
-		cout << endl;
+		print_result(*flight1, *closest);
+	}
+}
+
+int main()
+{
+	vector< Flight > flights = parseFile("data/20170901_080005.csv");
+
+	if (flights.size() < 2) {
+		// No reason to calculate distances
+		return 0;
 	}
 
-	cout << flights.size() << endl;
+	SolveWithForForLoop(move(flights));
+
     return 0;
 }
 
